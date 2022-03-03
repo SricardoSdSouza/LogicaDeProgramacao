374162efc1ab5599c7ee7e03f47fc58b69d2705c
SricardoSdSouza <sricardosdsouza@yahoo.com>
2022-03-02 22:45:49 -0300

    Novos exercícios

---
+++ MaiorEPosicao1080.py
@@ -0,0 +1,13 @@
+'''
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Entrada = O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+Saída = Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+Exemplo de Entrada	                Exemplo de Saída
+2                                       34565
+113                                     4
+45
+34565
+6
+...
+8
+'''
